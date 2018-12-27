6746c4b4705b09c3c7280d88d5b2463a8375cf52
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-12-26 23:00:37 -0700

    get properties

--- web/src/main/java/WebBaseClass.java
+++ web/src/main/java/WebBaseClass.java
@@ -8,45 +8,62 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.Map;
 import java.util.Properties;
+import java.util.TreeMap;
 
 public class WebBaseClass {
     public WebDriver driver = null;
     static InputStream input;
     static Properties prop = new Properties();
-    static WebCapabilities webCapabilities = null;
+    static WebCap webCap = null;
 
     static {
         try {
             System.out.println(System.getProperty("user.dir"));
-            input = new FileInputStream("/Users/umahaea/Documents/workspace/full-stack-automation/web/enviornment.properties");
+            input = new FileInputStream(
+                "/Users/umahaea/Documents/workspace/full-stack-automation/web/enviornment.properties");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws IOException {
+    @BeforeClass(alwaysRun = true) public void setUp() throws IOException {
         prop.load(input);
-        System.out.println(prop.size());
-        Enumeration keys = prop.keys();
+        Enumeration propertyName = prop.keys();
+        TreeMap<String, String> treeMap = new TreeMap<String, String>();
 
-        while (keys.hasMoreElements()) {
-            String key = (String)keys.nextElement();
-            String value = (String)prop.get(key);
+        while (propertyName.hasMoreElements()) {
+            String key = (String) propertyName.nextElement();
+            String value = (String) prop.get(key);
             System.out.println(key + ": " + value);
+            treeMap.put(key, value);
+        }
+        //System.out.println(treeMap);
+        String[] capabilities = new String[treeMap.size()];
+
+        for (int i = 0; i < treeMap.size(); i++) {
+            capabilities[i] = treeMap.values().toArray()[i].toString();
+            System.out.println(capabilities[i]);
         }
 
+        webCap = new WebCap.WebCapBuilder().
+            setBrowser(capabilities[0]).
+            setOs(capabilities[1]).
+            setOsVersion(capabilities[2]).
+            setVersion(capabilities[3]).
+            build();
+
+
         
-        webCapabilities =
-            new WebCapabilities(prop.getProperty("browser"), prop.getProperty("version"),
-                prop.getProperty("os"), prop.getProperty("osVersion"));
-        System.setProperty("webdriver.chrome.driver", "/Users/umahaea/Documents/workspace/full-stack-automation/drivers/chromedriver");
+
+
+        System.setProperty("webdriver."+capabilities[0]+".driver",
+            "/Users/umahaea/Documents/workspace/full-stack-automation/drivers/chromedriver");
         driver = new ChromeDriver();
     }
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() {
+    @AfterClass(alwaysRun = true) public void tearDown() {
         driver.close();
         driver.quit();
     }
