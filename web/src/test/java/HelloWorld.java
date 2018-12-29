4c54fdf69df8f07053f3f7b400d86258100af976
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-12-29 09:39:40 -0700

    add tests

--- web/src/test/java/HelloWorld.java
+++ web/src/test/java/HelloWorld.java
@@ -7,13 +7,17 @@ public class HelloWorld extends WebBaseClass {
 
     @Test public void test1() {
         automate.getUrl("https://www.pearson.com/us/");
+        System.out.println("1: "+automate.isElementDisplayed(By.className("search-box-icon")));
+        System.out.println("1: "+automate.isElementDisplayed(By.className("search-box-input")));
         automate.sendKeys(By.className("search-box-input"), "");
+
         automate.click(By.className("search-box-icon"));
     }
 
 
     @Test public void test2() {
         automate.getUrl("https://www.pearson.com/us/");
+        System.out.println("3: "+automate.isElementDisplayed(By.xpath("//*[@id=\"top\"]/footer/div/div/section[2]/div/div/div[3]/div/ul/li[2]/a")));
         automate.click(By.xpath("//*[@id=\"top\"]/footer/div/div/section[2]/div/div/div[3]/div/ul/li[2]/a"));
     }
 }
