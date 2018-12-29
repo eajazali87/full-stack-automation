a099c49cfcae643ad00cb8bdcb0cfa877460fce0
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-12-29 00:02:03 -0700

    to do

--- web/src/main/java/WebBaseClass.java
+++ web/src/main/java/WebBaseClass.java
@@ -127,10 +127,10 @@ public class WebBaseClass {
 }
 
 // -> Hook it up with Travis CI and Jenkins
-// -> Set up a run time environment in jenkins (for eg chrome)
 // -> Sauce connect set up
 // -> report integration
 // -> emailable report
+// -> send screen shot on failure
 // -> Start work on moving common code to shared services
 // -> Add some exception handling cases for incorrect values in the properties files
 // -> Add few more unit tests
