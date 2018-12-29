4842ac143053b0f5b676ee9e598a52db7ea74bf1
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-12-29 09:34:22 -0700

    add tests

--- web/src/test/java/HelloWorld.java
+++ web/src/test/java/HelloWorld.java
@@ -1,11 +1,19 @@
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class HelloWorld extends WebBaseClass{
+public class HelloWorld extends WebBaseClass {
 
-    @Test
-    public void test1() throws IOException {
-        automate.getUrl("http://www.yahoo.com");
+    @Test public void test1() {
+        automate.getUrl("https://www.pearson.com/us/");
+        automate.sendKeys(By.className("search-box-input"), "");
+        automate.click(By.className("search-box-icon"));
+    }
+
+
+    @Test public void test2() {
+        automate.getUrl("https://www.pearson.com/us/");
+        automate.click(By.xpath("//*[@id=\"top\"]/footer/div/div/section[2]/div/div/div[3]/div/ul/li[2]/a"));
     }
 }
